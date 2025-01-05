a5edc21bf965bb376836e8bc1423d6036d105ee9
kamathis4 <kamathis4@gmail.com>
2025-01-05 12:54:48 +0530

    Modify deployment config and fix setup

--- babyagi_task_executor/run.py
+++ babyagi_task_executor/run.py
@@ -2,10 +2,10 @@
 from dotenv import load_dotenv
 from babyagi_task_executor.schemas import InputSchema, TaskExecutorPromptSchema, TaskExecutorAgentConfig
 import json
-from litellm import completion
 import os
 from naptha_sdk.schemas import AgentDeployment, AgentRunInput
 from naptha_sdk.utils import get_logger
+import asyncio
 
 load_dotenv()
 
@@ -15,50 +15,59 @@ class TaskExecutorAgent:
     def __init__(self, agent_deployment: AgentDeployment):
         self.agent_deployment = agent_deployment
 
-    def execute_task(self, inputs: InputSchema):
-        if isinstance(self.agent_deployment.agent_config, dict):
-            self.agent_deployment.agent_config = TaskExecutorAgentConfig(**self.agent_deployment.agent_config)
-
-        user_prompt = self.agent_deployment.agent_config.user_message_template.replace("{{task}}", inputs.tool_input_data.task).replace("{{objective}}", inputs.tool_input_data.objective)
+    async def execute_task(self, inputs: InputSchema):
+        if isinstance(self.agent_deployment.config, dict):
+            self.agent_deployment.config = TaskExecutorAgentConfig(**self.agent_deployment.config)
+        
+        user_message_template = "You are given the following task: {{task}}. The task is to accomplish the following objective: {{objective}}."
+        user_prompt = user_message_template.replace("{{task}}", inputs.tool_input_data.task).replace("{{objective}}", inputs.tool_input_data.objective)
 
         messages = [
-            {"role": "system", "content": json.dumps(self.agent_deployment.agent_config.system_prompt)},
+            {"role": "system", "content": json.dumps(self.agent_deployment.config.system_prompt)},
             {"role": "user", "content": user_prompt}
         ]
 
-        api_key = None if self.agent_deployment.agent_config.llm_config.client == "ollama" else ("EMPTY" if self.agent_deployment.agent_config.llm_config.client == "vllm" else os.getenv("OPENAI_API_KEY"))
+        llm_config = self.agent_deployment.config.llm_config
 
-        response = completion(
-            model=self.agent_deployment.agent_config.llm_config.model,
-            messages=messages,
-            temperature=self.agent_deployment.agent_config.llm_config.temperature,
-            max_tokens=self.agent_deployment.agent_config.llm_config.max_tokens,
-            api_base=self.agent_deployment.agent_config.llm_config.api_base,
-            api_key=api_key
+        input_ = {
+            "messages": messages,
+            "model": llm_config.model,
+            "temperature": llm_config.temperature,
+            "max_tokens": llm_config.max_tokens
+        }
+
+        response = await naptha.node.run_inference(
+            input_
         )
 
-        response = response.choices[0].message.content
-        logger.info(f"Response: {response}")
+        try:
+            response_content = response.choices[0].message.content
+            return response_content
+        
+        except Exception as e:
+            logger.error(f"Failed to parse response: {response}. Error: {e}")
+            return
 
-        return response
-
-def run(agent_run: AgentRunInput, *args, **kwargs):
+async def run(agent_run: AgentRunInput, *args, **kwargs):
     logger.info(f"Running with inputs {agent_run.inputs.tool_input_data}")
 
-    task_executor_agent = TaskExecutorAgent(agent_run.agent_deployment)
+    task_executor_agent = TaskExecutorAgent(agent_run.deployment)
     method = getattr(task_executor_agent, agent_run.inputs.tool_name, None)
 
-    return method(agent_run.inputs)
+    return await method(agent_run.inputs)
 
 
 if __name__ == "__main__":
     from naptha_sdk.client.naptha import Naptha
-    from naptha_sdk.configs import load_agent_deployments
+    from naptha_sdk.configs import setup_module_deployment
+    import os
 
     naptha = Naptha()
 
     # Configs
-    agent_deployments = load_agent_deployments("babyagi_task_executor/configs/agent_deployments.json", load_persona_data=False, load_persona_schema=False)
+    deployment = asyncio.run(setup_module_deployment("agent", "babyagi_task_executor/configs/deployments.json", node_url = os.getenv("NODE_URL")))
+    deployment = AgentDeployment(**deployment.model_dump())
+    print("BabyAGI Task Executor Deployment:", deployment)
 
     input_params = InputSchema(
         tool_name="execute_task",
@@ -67,8 +76,9 @@ if __name__ == "__main__":
 
     agent_run = AgentRunInput(
         inputs=input_params,
-        agent_deployment=agent_deployments[0],
+        deployment=deployment,
         consumer_id=naptha.user.id,
     )
 
-    response = run(agent_run)
+    response = asyncio.run(run(agent_run))
+    logger.info(f"Final Response: {response}")
